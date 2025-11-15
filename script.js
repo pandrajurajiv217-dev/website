fae262ee8824c5582e916dcb18b06d5395f0e67f
PANDRAJU RAJIV <pandrajurajiv217@gmail.com>
2025-11-15 17:02:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple JS interaction for demo
+document.getElementById("loginBtn").addEventListener("click", function() {
+  alert("Login functionality coming soon!");
+});
+
+document.getElementById("searchBtn").addEventListener("click", function() {
+  alert("Searching for available flights...");
+});
